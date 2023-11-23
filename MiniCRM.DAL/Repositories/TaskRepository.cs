5e7534e657874606951f27a674ce16a969b94544
AlexanderLukashuk <upiter465@gmail.com>
2023-11-23 15:17:02 +0600

    Created TaskRepository which implemented IBaseRepository interface

---
+++ MiniCRM.DAL/Repositories/TaskRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiniCRM.DAL.Entity;
+using MiniCRM.DAL.Interfaces;
+
+namespace MiniCRM.DAL.Repositories
+{
+    public class TaskRepository : IBaseRepository<TaskEntity>
+    {
+        private readonly AppDbContext _context;
+
+        public TaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(TaskEntity entity)
+        {
+            await _context.Tasks.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(TaskEntity entity)
+        {
+            _context.Tasks.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<TaskEntity> GetAll()
+        {
+            return _context.Tasks.AsQueryable();
+        }
+
+        public async Task<TaskEntity> Update(TaskEntity entity)
+        {
+            _context.Tasks.Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
