6a5f0d5d155c4d9a4bbb035d5c64c55bee88d88e
AlexanderLukashuk <upiter465@gmail.com>
2023-12-07 00:24:14 +0600

    Created TaskController

---
+++ MiniCRM/Controllers/TaskController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MiniCRM.BLL.Interfaces;
+using MiniCRM.BLL.Models;
+
+namespace MiniCRM.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet("{employeeId}")]
+        public async Task<ActionResult<IEnumerable<Task>>> GetTasksBYEmployeeId(int employeeId)
+        {
+            var tasks = await _taskService.GetTasksByEmployeeId(employeeId);
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskBLL>> GetTaskById(int id)
+        {
+            var task = await _taskService.GetTaskById(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskBLL>> CreateTask(TaskBLL task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _taskService.AddTask(task);
+            return CreatedAtAction(nameof(GetTaskById), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TaskBLL>> UpdateTask(int id, TaskBLL task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _taskService.UpdateTask(task);
+
+            return Ok(task);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteTask(int id)
+        {
+            await _taskService.DeleteTask(id);
+
+            return NoContent();
+        }
+    }
+}
