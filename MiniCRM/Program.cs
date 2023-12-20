110f5049abae979a7f4a78fe04a618f692228e8d
AlexanderLukashuk <upiter465@gmail.com>
2023-12-20 19:24:38 +0600

    Created Initializer to initialize repositories and services

--- MiniCRM/Program.cs
+++ MiniCRM/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using MiniCRM;
 using MiniCRM.DAL;
 using MiniCRM.Middleware;
 using Serilog;
@@ -22,6 +23,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(connectionString, optionsBuilder => optionsBuilder.MigrationsAssembly("MiniCRM.DAL")));
+
+builder.Services.InitializeRepositories();
+builder.Services.InitializeServices();
 
 // builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 //     .AddEntityFrameworkStores<AppDbContext>()
