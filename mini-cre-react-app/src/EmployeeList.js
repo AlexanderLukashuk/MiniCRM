c2e93716d99888cb0aa0404caf81412176ac7eca
AlexanderLukashuk <upiter465@gmail.com>
2023-12-22 14:30:16 +0600

    Changed the name display

--- mini-cre-react-app/src/EmployeeList.js
+++ mini-cre-react-app/src/EmployeeList.js
@@ -2,30 +2,33 @@ import React, { useState, useEffect } from 'react';
 import AddEmplloyee from './AddEmployee';
 
 const EmployeeList = () => {
-  const [employees, setEmployees] = useState([]);
+    const [employees, setEmployees] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5084/api/employee')
-      .then(response => response.json())
-      .then(data => setEmployees(data))
-      .catch(error => console.error('Error fetching employees:', error));
-  }, []);
+    useEffect(() => {
+        fetch('http://localhost:5084/api/employee')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setEmployees(data);
+            })
+            .catch(error => console.error('Error fetching employees:', error));
+    }, []);
 
-  const handleAddEmployee = newEmployee => {
-    setEmployees([...employees, newEmployee]);
-  }
+    const handleAddEmployee = newEmployee => {
+        setEmployees([...employees, newEmployee]);
+    }
 
-  return (
-    <div>
-      <h2>Employee List</h2>
-      <ul>
-        {employees.map(employee => (
-          <li key={employee.id}>{employee.name}</li>
-        ))}
-      </ul>
-      <AddEmplloyee onAddEmployee={handleAddEmployee} />
-    </div>
-  );
+    return (
+        <div>
+            <h2>Employee List</h2>
+            <ul>
+                {employees.map(employee => (
+                    <li key={employee.id}>{employee.fullName}</li>
+                ))}
+            </ul>
+            <AddEmplloyee onAddEmployee={handleAddEmployee} />
+        </div>
+    );
 };
 
 export default EmployeeList;
