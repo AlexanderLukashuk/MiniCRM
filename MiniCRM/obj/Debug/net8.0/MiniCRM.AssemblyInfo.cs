387f01c928b9ec8a544fe8342d06eb391be25a17
AlexanderLukashuk <upiter465@gmail.com>
2023-12-22 14:07:14 +0600

    Building

--- MiniCRM/obj/Debug/net8.0/MiniCRM.AssemblyInfo.cs
+++ MiniCRM/obj/Debug/net8.0/MiniCRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efca772e95b2f000d492c6ed14aebf4c4573e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
