e8a8515896dfa8d167509fcbc53be28b769a13f3
AlexanderLukashuk <upiter465@gmail.com>
2024-01-06 08:45:22 +0600

    Added employee id and task id, changed setting task properties and html-form

--- mini-cre-react-app/src/EditTaskPage.js
+++ mini-cre-react-app/src/EditTaskPage.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const EditTaskPage = () => {
-    const [taskName, setTaskName] = useState('');
+    const params = useParams();
+    const { taskId } = params;
+    const { id } = useParams();
+    const [title, setTitle] = useState('');
+    const [taskDescription, setTaskDescription] = useState('');
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [completionPercentage, setCompletionPercentage] = useState('');
     const navigate = useNavigate();
-    const { taskId } = useParams();
 
     const handleEditTask = () => {
         fetch(`http://localhost:5084/api/task/${taskId}`, {
@@ -15,12 +18,12 @@ const EditTaskPage = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ taskName, startDate, endDate, completionPercentage }),
+            body: JSON.stringify({ title, taskDescription, startDate, endDate, completionPercentage, employeeId: id }),
         })
             .then(response => response.json())
             .then(data => {
                 // console.log('Task edited:', data);
-                navigate(`/task/${taskId}`);
+                navigate("/");
             })
             .catch(error => console.error('Error editing task', error));
     };
@@ -29,7 +32,9 @@ const EditTaskPage = () => {
         <div>
             <h2>Edit Task</h2>
             <label>Task Name:</label>
-            <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <label>Task Description:</label>
+            <input type="text" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
             <label>Start Date:</label>
             <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
             <label>End Date:</label>
